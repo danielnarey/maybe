27d0a3bbeb2bd103c13dcd3dfcb77c6b71b88ec3
danielnarey <accounts@danielnarey.com>
2019-09-15 05:01:26 +0000

    automated sync

--- src/of.js
+++ src/of.js
@@ -1,7 +1,7 @@
 /* ### `of(v) => (?v)`
- * Create a **maybe** of something: perhaps a resolvable value, or perhaps a 
+ * Create a **maybe** of something: perhaps a resolvable value, or perhaps a
  * reference or function result that is `undefined`, `null`, an inappropriate
- * type, or out of bounds. Returns a functional interface to the enclosed value 
+ * type, or out of bounds. Returns a functional interface to the enclosed value
  * (denoted as `(?v)`).
 */
 export default (v) => (f) => f(v);
